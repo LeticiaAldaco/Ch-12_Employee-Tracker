3eb49ac8eeedb361179d1c3ac60456cc9aacecef
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-06-10 18:53:43 -0500

    server.js file started

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "employee_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+});
+
+module.exports = connection;
